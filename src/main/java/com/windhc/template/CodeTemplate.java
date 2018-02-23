75ab7695e0056e9e4dac8fbb314e955fe3b599a9
hc <1440557925@qq.com>
2018-02-23 14:09:50 +0800

    add Generator Action and plugin.xml

---
+++ src/main/java/com/windhc/template/CodeTemplate.java
@@ -0,0 +1,35 @@
+package com.windhc.template;
+
+/**
+ * @author windhc
+ */
+public interface CodeTemplate {
+
+    String CONTRACT_TEMPLATE =
+            "package %s.contract;\n" +
+                    "\n" +
+                    "/**\n * Created by MvpGenerator.\n */\n" +
+                    "public interface %sContract {\n" +
+                    "interface View extends IView {\n" +
+                    "}\n" +
+                    "\n" +
+                    "interface Presenter extends IPresenter<View> {\n" +
+                    "}\n" +
+                    "\n" +
+                    "interface Model extends IModel {\n" +
+                    "}\n" +
+                    "}";
+
+    String PRESENTER_TEMPLATE = "package %s.presenter;\n" +
+            "\n" +
+            "/**\n * Created by MvpGenerator.\n */\n" +
+            "public class %sPresenter extends BasePresenter<%sContract.View, %sContract.Model> implements %sContract.Presenter {\n" +
+            "}";
+
+    String MODEL_TEMPLATE = "package %s.model;\n" +
+            "\n" +
+            "/**\n * Created by MvpGenerator for CreditCard.\n */\n" +
+            "public class %sModel implements %sContract.Model {\n" +
+            "}";
+
+}
